9bab3cac55fdd83e73289f64ce78e28bf30b1626
aissa <aissa@ARQ>
2019-02-28 23:32:55 +0000

    JaxRsActivator On

--- src/main/java/jersey101/JaxrsActivator.java
+++ src/main/java/jersey101/JaxrsActivator.java
@@ -6,12 +6,12 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/*@ApplicationPath("/rest")*/
-public class JaxrsActivator/* extends Application*/ {
+@ApplicationPath("/rest")
+public class JaxrsActivator extends Application {
 	
-     public Set<Class<?>> getClasses() {
-         Set<Class<?>> s = new HashSet<Class<?>>();
-         s.add(Jersey.class);
-         return s;
-     } 
+//     public Set<Class<?>> getClasses() {
+//         Set<Class<?>> s = new HashSet<Class<?>>();
+//         s.add(Jersey.class);
+//         return s;
+//     } 
 }
